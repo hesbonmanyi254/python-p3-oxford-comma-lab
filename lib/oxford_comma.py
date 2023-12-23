d1ffb0bfe513221f693b0caa23cba4e90db64de7
hesbonmanyi254 <hesbonmanyinsa96@gmail.com>
2023-12-23 17:08:13 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+# oxford_comma.py
+
 def oxford_comma(items):
-    return None
+    '''Return a string with proper Oxford comma formatting.'''
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        # Join all elements with commas, except the last one, which is joined with "and"
+        return f"{', '.join(items[:-1])}, and {items[-1]}"
